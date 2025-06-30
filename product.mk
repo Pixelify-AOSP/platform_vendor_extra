8e33f38b33f80d9d80036493d5611e7bf049df2e
rmp22 <195054967+rmp22@users.noreply.github.com>
2025-07-01 05:14:46 +0800

    extra: Update blur support
    Change-Id: I1fa7eec24a498395950fc6e377ccbca802a50d0b

--- product.mk
+++ product.mk
@@ -14,11 +14,9 @@ TARGET_ENABLE_BLUR ?= true
 ifeq ($(TARGET_ENABLE_BLUR),true)
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.blur.enable=true \
-    persist.sysui.disableBlur=false \
     ro.surface_flinger.supports_background_blur=1
 else
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.custom.blur.enable=false \
-    persist.sysui.disableBlur=true \
     ro.surface_flinger.supports_background_blur=0
 endif
