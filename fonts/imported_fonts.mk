44eb2e445d5a83d39239a92cb2d9091b53bc7769
kumarshailesh-codes <kumarshailesh12qw4@gmail.com>
2026-07-15 14:36:12 +0000

    fonts: Resolve duplicate target override errors
    Filter out duplicate fonts from PRODUCT_COPY_FILES if they are already built/installed via PRODUCT_PACKAGES (Soong).

--- fonts/imported_fonts.mk
+++ fonts/imported_fonts.mk
@@ -1,3 +1,5 @@
 # Evolution X imported fonts
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,vendor/extra/fonts/imported,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+imported_font_pairs := $(call find-copy-subdir-files,*,vendor/extra/fonts/imported,$(TARGET_COPY_OUT_PRODUCT)/fonts)
+
+# Filter out duplicate fonts that are already built/installed via PRODUCT_PACKAGES (Soong)
+PRODUCT_COPY_FILES += $(filter-out $(foreach f,$(PRODUCT_PACKAGES),%/$(f)),$(imported_font_pairs))
